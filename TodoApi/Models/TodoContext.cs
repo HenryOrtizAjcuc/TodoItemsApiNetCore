c8bafdd8f5a496ac3c607656c7efc3d4daba9bfc
IT2 Henry <itprogintelaf2@outlook.com>
2020-11-28 22:28:16 -0600

    implementación del tutorial TodoItems Api .net core

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        // En ASP.NET Core, los servicios (como el contexto de la base de datos) deben registrarse
+        // con el contenedor de inserción de dependencias (DI). El Contenedor proporciona el
+        // servicio a los controladores.
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
